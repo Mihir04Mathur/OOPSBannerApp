f1c74c005ff49c107656c19217fb77250c10dc79
Mihir Mathur <mihir04mathur@gmail.com>
2026-02-17 22:12:01 +0530

    Implement UC1: Print OOPS to console

---
+++ OOPSBannerApp.java
@@ -0,0 +1,18 @@
+/**
+ * OOPS Banner App - Prints a banner to the console.
+ * Use Case 1: Printing the literal text "OOPS" to the console.
+ * 
+ * @author YourName
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    /**
+     * The main entry point of the application.
+     * 
+     * @param args Command line arguments (not used).
+     */
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
